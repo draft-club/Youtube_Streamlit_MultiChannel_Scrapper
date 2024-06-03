a1b203e571b8d55ebab6963c57891995966af418
draft-club <hicker.coder.2025@gmail.com>
2024-06-03 12:01:33 +0200

    Test app

---
+++ styles.py
@@ -0,0 +1,23 @@
+NAVBAR="""
+         <style>
+             .navbar {
+                 background-color: blue;
+                 padding: 10px 0;
+                 text-align: center;
+             }
+             .navbar a {
+                 color: white;
+                 margin: 0 15px;
+                 padding: 10px;
+                 text-decoration: none;
+             }
+             .navbar a:hover {
+                 background-color: darkblue;
+                 border-radius: 5px;
+             }
+         </style>
+         <div class="navbar">
+             <a href="/" target="https://www.finances.gov.ma">Visit Our Website</a>
+             <a href="mailto:amin.mba@iuj.ac.jp">Get In Touch</a>
+         </div>
+         """
